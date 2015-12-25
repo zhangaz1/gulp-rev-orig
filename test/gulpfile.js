53af7e2f1a51a236ffc2714688b6fdc145016b39
zhangaz1 <zhangaz1@hotmail.com>
2015-12-25 17:43:59 +0800

    注释测试输出

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -3,8 +3,6 @@ var util = require('gulp-util')
 var revOrig = require('./../index.js');
 
 gulp.task('revOrig', function(argument) {
-    console.log(util.env.dir);
-
     gulp.src('test.html')
         .pipe(revOrig())
         .pipe(gulp.dest('./dist'))
