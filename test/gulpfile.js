e06bef906df51d93bcdc9589c6c13baec2aa937a
zhangaz1 <zhangaz1@hotmail.com>
2015-12-25 13:35:01 +0800

    使用uitl.env

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -1,8 +1,11 @@
-var gulp = require("gulp");
-var revOrig = require("./../index.js");
+var gulp = require('gulp');
+var util = require('gulp-util')
+var revOrig = require('./../index.js');
 
-gulp.task("revOrig", function(argument) {
-    gulp.src("test.html")
+gulp.task('revOrig', function(argument) {
+    console.log(util.env.dir);
+
+    gulp.src('test.html')
         .pipe(revOrig())
-        .pipe(gulp.dest("./dist"))
+        .pipe(gulp.dest('./dist'))
 });
